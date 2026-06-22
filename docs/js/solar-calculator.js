5c855396f201c5aa3a05ca10084dda11d5364d7d
Bruno Arine <bruno.arine@runbox.com>
2026-06-21 22:31:46 -0300

    Change map tile layer from CARTO dark to OpenStreetMap Mapnik

--- docs/js/solar-calculator.js
+++ docs/js/solar-calculator.js
@@ -83,9 +83,8 @@
 
         map = L.map('dark-days-map').setView([-14.235, -51.925], 4);
 
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
-            subdomains: 'abcd',
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
             maxZoom: 19
         }).addTo(map);
 
